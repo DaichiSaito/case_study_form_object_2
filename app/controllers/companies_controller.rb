5881bdac4ca5b7a57fd179bb5618c0b19e6e69f0
DaichiSaito <hrkedz@gmail.com>
2021-05-30 16:51:23 +0900

    企業とユーザーの同時登録フォーム作成

---
+++ app/controllers/companies_controller.rb
@@ -0,0 +1,24 @@
+class CompaniesController < ApplicationController
+  def index
+    @companies = Company.all
+  end
+
+  def new
+    @signup_form = SignupForm.new
+  end
+
+  def create
+    @signup_form = SignupForm.new(signup_params)
+    if @signup_form.save
+      redirect_to companies_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def signup_params
+    params.require(:signup_form).permit(company_attributes: %i[name address], user_attributes: %i[name email password password_confirmation])
+  end
+end
